374deea5f87d93a35f49c2e07355cf4f755c64bb
tjlinch <Tyler@Tylers-MacBook-Pro-8.local>
2022-09-01 00:07:17 -0600

    wrote tests for engineer constructor, wrote engineer constructor as a subclass of Employee

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,40 @@
 const Engineer = require('../lib/engineer');
+
+describe('Engineer', () => {
+    describe('Initialization', () => {
+        it('should create an object containing the "github" property, as well as the properties inherited from the Employee class', () => {
+            const github = 'tjlinch';
+
+            const obj = new Engineer('Tyler', '47', 'tyler@email.com', github);
+
+            expect(obj.github).toEqual(github);
+        });
+
+        it('should throw an error if not provided a value for the github property', () => {
+            const cb = () => new Engineer();
+            const err = new Error(
+                'Expected parameter "github" to be a non empty string'
+            );
+
+            expect(cb).toThrowError(err);
+        });
+    });
+
+    describe('getGithub', () => {
+        it('should get the engineer github', () => {
+            const github = 'tjlinch';
+
+            const obj = new Engineer ('Tyler', '47', 'tyler@email.com', github);
+            
+            expect(obj.getGithub()).toEqual(github);
+        });
+    });
+
+    describe('getRole', () => {
+        it('should return the engineers role of "Engineer"', () => {
+            const obj = new Engineer ('Tyler', '01', 'tyler@email.com', 'tjlinch');
+
+            expect(obj.getRole()).toEqual('Engineer');
+        });
+    });
+});
