eab9a65af41b2237dbbf574c0d3bba5256733918
tjlinch <Tyler@Tylers-MacBook-Pro-8.local>
2022-08-31 22:57:31 -0600

    finished test for Employee constructor, created the actual constructor function

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,32 +2,80 @@ const Employee = require('../lib/employee');
 
 describe('Employee', () => {
     describe('Initialization', () => {
+        // Positive test
         it('should create an object containing "name", "id", and "email" properties set to the arguments provided when called with the "new" keyword.', () => {
+            //Arrange
             const name = 'Tyler';
-            const id = 01;
+            const id = '01';
             const email = 'tyler@email.com';
 
+            // Act
             const obj = new Employee(name, id, email);
 
+            // Assert
             expect(obj.name).toEqual(name);
             expect(obj.id).toEqual(id);
             expect(obj.email).toEqual(email);
         });
-        
-        // it('should create an object containing an "id" property set to the "id" argument provided when called with the " when called with the "new" keyword.', () => {
-        //     const obj = new Employee();
 
-        //     expect('id' in obj).toEqual(true);
-        // });
+        // Exception test
+        it('should throw an error if not provided a value for any of the properties', () => {
+            //Arrange
+            const cb = () => new Employee();
+            const err = new Error(
+                'Expected parameters "name", "id", and "email" to be non empty strings'
+            );
 
-        // it('should return an object containing an "email" property when called with the "new" keyword.', () => {
-        //     const obj = new Employee();
-
-        //     expect('email' in obj).toEqual(true);
-        // });
+            //Assert
+            expect(cb).toThrowError(err);
+        });
     });
 
     describe('getName', () => {
-        it('should')
-    })
-})
+        it('should get the employee name', () => {
+            const name = 'Tyler';
+            const id = '47';
+            const email = 'tyler@email.com';
+            
+            const obj = new Employee(name, id, email);
+
+            expect(obj.getName()).toEqual(name);
+        });
+    });
+
+    describe('getId', () => {
+        it('should get the employee id', () => {
+            const name = 'Tyler';
+            const id = '47';
+            const email = 'tyler@email.com';
+            
+            const obj = new Employee(name, id, email);
+
+            expect(obj.getId()).toEqual(id);
+        });
+    });
+
+    describe('getEmail', () => {
+        it('should get the employee email', () => {
+            const name = 'Tyler';
+            const id = '47';
+            const email = 'tyler@email.com';
+            
+            const obj = new Employee(name, id, email);
+
+            expect(obj.getEmail()).toEqual(email);
+        });
+    });
+
+    describe('getRole', () => {
+        it('should return the employees role of "Employee"', () => {
+            const name = 'Tyler';
+            const id = '47';
+            const email = 'tyler@email.com';
+            
+            const obj = new Employee(name, id, email);
+
+            expect(obj.getRole()).toEqual('Employee');
+        });
+    });
+});
