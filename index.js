478af12993f6056975f185ee10f64464e4e105c7
tjlinch <Tyler@Tylers-MacBook-Pro-8.local>
2022-09-01 11:48:55 -0600

    switched inquirer to v. 8.2.4

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-const generate HTML = require('./src/template');
+const generateHTML = require('./src/template');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const teamArray = [];
+
+function initApp () {
+
+    function createTeam () {
+        inquirer.prompt(
+            [{
+                type: 'list',
+                message: 'Which employee would you like to create?',
+                name: 'employeeRole',
+                choices: ['Manager', 'Engineer', 'Intern']
+            }
+        ]
+        ).then(function (input) {
+            switch(input.employeeRole) {
+                case 'Manager':
+                    addManager();
+                    break;
+            }
+        })
+    }
+    createTeam();
+}
+
+
+initApp();
